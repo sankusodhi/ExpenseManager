0d7549410e013b1ee5fde62061caf30819777a47
sankusodhi <sankuramsodhi23@navgurukul.org>
2024-10-06 23:34:29 +0530

    Add Expense Claim DocType and related files

---
+++ expensemanager/manage_expense/doctype/expense_claim/test_expense_claim.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2024, sanku and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests import IntegrationTestCase, UnitTestCase
+
+
+class TestExpenseClaim(UnitTestCase):
+	"""
+	Unit tests for ExpenseClaim.
+	Use this class for testing individual functions and methods.
+	"""
+
+	pass
+
+
+class TestExpenseClaim(IntegrationTestCase):
+	"""
+	Integration tests for ExpenseClaim.
+	Use this class for testing interactions between multiple components.
+	"""
+
+	pass
